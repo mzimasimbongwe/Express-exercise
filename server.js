d783dccf58505ff8fef547f0fd311bffe000ed80
mzimasimbongwe <Snezoh97>
2023-10-04 21:49:57 +0200

    ..

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require ('express')
 const app = express()
 
 app.set('view engine', 'ejs')
+
 app.get("/", (req, res) => {
     console.log("Here")
-    res.render('index')
+    res.render("index")
 })
 
 app.listen(3000)
